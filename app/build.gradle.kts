fc69b222b4eebf6b2581361c6b9fce185bcba4cf
Repo Master <repomaster@local>
2026-09-13 20:04:04 +0300

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -46,6 +46,6 @@ dependencies {
     implementation("androidx.activity:activity-compose:1.9.2")
     implementation("androidx.core:core-ktx:1.13.1")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.8.6")
-
-    debugImplementation("androidx.compose.ui:ui-tooling")
+    implementation("com.google.android.material:material:1.11.0")
+   debugImplementation("androidx.compose.ui:ui-tooling")
 }
